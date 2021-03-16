b6b812182c9de34fd599baa41693b37c293581d4
Obaro I. Johnson <obaro.johnson@servotestystems.com>
2021-03-16 16:34:37 +0000

    Fixed setup.py file

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 import setuptools
+import os;
+
 long_description = "Simple JSON database";
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+readMEFile =  "README.md"
+if (os.path.exists(readMEFile)):
+  with open(readMEFile , "r") as fh:
+      long_description = fh.read()
     
 setuptools.setup(name = "jimo_repositories",
       version="1.0.0",
       description="Simple JSON database manager for python",
       long_description =long_description,
-      url="https://github.com/miljimo/repositories.git",
+      url="https://github.com/miljimo/jimo_repositories.git",
       long_description_content_type="text/markdown",
       author="Obaro I. Johnson",
       author_email="johnson.obaro@hotmail.com",
-      packages=setuptools.find_packages(),
+      packages=["jimo_repositories"],
       install_requires=[],
       classifiers=[
         "Programming Language :: Python :: 3",
