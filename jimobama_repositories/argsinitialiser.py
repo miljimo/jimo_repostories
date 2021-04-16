96cdfabd6baddc285acc8410c11016d1360e44db
Obaro I. Johnson <obaro.johnson@servotestystems.com>
2021-04-16 15:04:58 +0100

    Args initialiser refactory

--- jimobama_repositories/argsinitialiser.py
+++ jimobama_repositories/argsinitialiser.py
@@ -1,5 +1,5 @@
 import os
-
+import copy;
 
 """
  A class wrapper to specific defaults arguments and help to validate them base on the default types.
@@ -10,31 +10,36 @@ class argument_validator:
 
     def __init__(self, *args, **kwargs):
         #This arguments will be used to initial the class and its called once.
-        self.validator = ArgsInitialiser(*args, **kwargs)
-        
+        self.__defaults_kwargs  =  kwargs;
+        self.__defaults_args    =  args;        
+               
         pass
 
     def __call__(self, operation):
-        # validate a class parameters
-        validator = self.validator
+       
+        default_kwargs  = self.__defaults_kwargs;
+        defaults_args   = self.__defaults_args;
+        
         if(type(operation) == object.__class__):
+            
             class __ClsValidator(operation):
-
                 def __init__(self, *args, **kwargs):
-                    super().__init__(
-                        *validator.args, **validator.kwargs)                    
+                    self.__validator = ArgsInitialiser(*copy.copy(defaults_args), **copy.copy(default_kwargs));
+                    if(self.__validator.validate(*args, **kwargs)) is not True:
+                        raise TypeError("@unexpected type provided");
+                    super().__init__(*self.__validator.args, **self.__validator.kwargs)
 
-                def __new__(cls, *args, **kwargs):
-                    if(validator.validate(*args, **kwargs)):
-                        return super().__new__(cls)
-                    return
+                @property
+                def argument_validator(self):
+                    return self.__validator;
+                
             __ClsValidator.__name__ = operation.__name__
             return __ClsValidator
         else:
             if(callable(operation) is True):
                 def construct_operation_wrapper(*args, **kwargs):
                     if(validator.validate(*args, **kwargs)):
-                        return operation(*validator.args, **validator.kwargs)
+                        return operation(*copy.deepcopy(validator.args), **copy.deepcopy(validator.kwargs))
                 return construct_operation_wrapper;
 
 
@@ -50,11 +55,11 @@ class ArgsInitialiser(object):
 
         self.args    = args;
         self.kwargs  = kwargs;
-      
+        
+        
 
     def validate(self, *args, **kwargs):
-        self.args  =  args;
-        
+        self.args      =  args;
         for key in kwargs:           
             if(key in self.kwargs.keys()) is not True:
                 raise ValueError(
@@ -73,21 +78,18 @@ class ArgsInitialiser(object):
 if __name__ == "__main__":
 
 
-    @argument_validator(age=89, name="Obaro")
+    @argument_validator(age=89, name="DefaultName", working=False)
     class __Person(object):
         def __init__(self, filename:str, **kwargs):
             self.Name = kwargs['name']
             self.Age = kwargs['age']
+            self.working = kwargs['working']
 
-    @argument_validator(age=89, name="Obaro")
-    def create_person(**kwargs):
-        print(kwargs);
-
-
-    create_person(age=190, name="John");
+  
            
     filename =  "Obaro.data"
-    p = __Person(filename, name="Johnson")
-    p2 = __Person(filename)
-    print(p.Name)
+    p =  __Person(filename, name="Johnson", working=True)
+    p2 = __Person(filename, name="Obaro")
+    p3 = __Person(filename, name="Obaro 2")
+    print(p2.working)
     
